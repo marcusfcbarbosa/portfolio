b21f154cfa8c80b7b04909ea9555535380209cba
marcus fernando <marcusfcbarbosa@gmail.com>
2018-06-29 00:23:29 -0300

    quarto dia alteracoes

--- src/components/Education/Component_Education.js
+++ src/components/Education/Component_Education.js
@@ -11,17 +11,15 @@ class Component_Education extends Component {
                                             <div className="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
                                             <div className="card-body cc-education-header">
                                                 <p>2013 - 2015</p>
-                                                <div className="h5">Master's Degree</div>
+                                                <div className="h5">Graduate studies</div>
                                             </div>
                                             </div>
                                             <div className="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
                                             <div className="card-body">
                                                 <div className="h5">Master of Information Technology</div>
-                                                <p className="category">University of Computer Science</p>
-                                                <p>Euismod massa scelerisque suspendisse fermentum habitant vitae ullamcorper magna quam iaculis,
-                                                    tristique sapien taciti mollis interdum sagittis libero nunc inceptos tellus, hendrerit vel eleifend primis lectus quisque
-                                                    cubilia sed mauris. Lacinia porta vestibulum diam integer quisque eros pulvinar curae, curabitur feugiat arcu vivamus parturient aliquet laoreet at,
-                                                    eu etiam pretium molestie ultricies sollicitudin dui.
+                                                <p className="category">University Center UNIBTA</p>
+                                                 <p>Software engineering and computer network projects.
+                                                 <a href="http://www.unibta.edu.br/" target="_blank"> UNIBTA </a>
                                                 </p>
                                             </div>
                                             </div>
@@ -33,17 +31,16 @@ class Component_Education extends Component {
                                             <div className="card-body cc-education-header">
                                                 <p>2009 - 2013</p>
                                                 <div className="h5">Bachelor's Degree</div>
+
+
                                             </div>
                                             </div>
                                             <div className="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
                                             <div className="card-body">
-                                                <div className="h5">Bachelor of Computer Science</div>
-                                                <p className="category">University of Computer Science</p>
-                                                <p>Euismod massa scelerisque suspendisse fermentum habitant vitae ullamcorper magna 
-                                                    quam iaculis, tristique sapien taciti mollis interdum sagittis libero nunc inceptos tellus,
-                                                    hendrerit vel eleifend primis lectus quisque cubilia sed mauris. Lacinia porta vestibulum diam 
-                                                    integer quisque eros pulvinar curae, curabitur feugiat arcu vivamus parturient aliquet laoreet at, 
-                                                    eu etiam pretium molestie ultricies sollicitudin dui.
+                                                <div className="h5">National Institute of Telecommunications (INATEL)</div>
+                                                <p className="category">University of Computer engineering</p>
+                                                <p>Course of analysis and development of systems, with intensive focus on calculation, physics, electronics, arithmetic, networks, algorithms, database.
+                                                <a href="http://www.inatel.br/home/" target="_blank"> INATEL </a>
                                                 </p>
                                             </div>
                                             </div>
@@ -53,18 +50,17 @@ class Component_Education extends Component {
                                         <div className="row">
                                             <div className="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
                                             <div className="card-body cc-education-header">
-                                                <p>2007 - 2009</p>
+                                                <p>2001 - 2004</p>
                                                 <div className="h5">High School</div>
                                             </div>
                                             </div>
                                             <div className="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
                                             <div className="card-body">
-                                                <div className="h5">Science and Mathematics</div>
+                                                <div className="h5">INTEGRAL ATIBAIA</div>
                                                 <p className="category">School of Secondary board</p>
-                                                <p>Euismod massa scelerisque suspendisse fermentum habitant vitae ullamcorper magna quam iaculis, 
-                                                    tristique sapien taciti mollis interdum sagittis libero nunc inceptos tellus, hendrerit vel eleifend primis 
-                                                    ectus quisque cubilia sed mauris. Lacinia porta vestibulum diam integer quisque eros pulvinar curae, curabitur 
-                                                    feugiat arcu vivamus parturient aliquet laoreet at, eu etiam pretium molestie ultricies sollicitudin dui.
+                                                <p>
+                                                    Basically high school in Brazil are widespread, we do not have a study directed at exact, human or biological ... we have a mixture of everything.
+                                                     <a href="http://integral.br/web/" target="_blank"> INTEGRAL </a> 
                                                 </p>
                                             </div>
                                             </div>
